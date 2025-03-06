5ae887c0a9a0cbb3932afa4a0b7bf921eadb671d
AndreDevFront <bitcoinberwian@gmail.com>
2025-03-06 17:34:15 -0300

    Mantendo app.run para testes locais

--- app.py
+++ app.py
@@ -1,58 +1,64 @@
 from flask import Flask, render_template, request, send_file
-import os
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
 import xml.etree.ElementTree as ET
-from io import StringIO
+from io import StringIO, BytesIO
 import xml.dom.minidom
 
 app = Flask(__name__)
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = 'minha-chave-secreta'
 
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
+# Formulário
+class UploadForm(FlaskForm):
+    xml_file = FileField('XML File')
+    submit = SubmitField('Enviar')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', download_link=None)
+    form = UploadForm()
+    download_file_content = None
+    download_filename = None
+    
+    if form.validate_on_submit():
+        file = form.xml_file.data
+        if not file.filename.endswith('.xml'):
+            return "Por favor, envie um arquivo XML!", 400
+        
+        original_filename = secure_filename(file.filename)
+        
+        # Ler o XML diretamente do upload
+        try:
+            xml_content = file.read().decode('utf-8')
+            tree = ET.ElementTree(ET.fromstring(xml_content))
+        except ET.ParseError:
+            return "Arquivo XML inválido!", 400
+        
+        root = tree.getroot()
+        
+        # Transformar em string sem formatação
+        xml_string = StringIO()
+        ET.ElementTree(root).write(xml_string, encoding='unicode', xml_declaration=True)
+        xml_string.seek(0)
+        
+        # Formatá-lo com minidom
+        xml_prettified = xml.dom.minidom.parseString(xml_string.getvalue())
+        formatted_xml = xml_prettified.toprettyxml(indent="  ")
+        
+        # Preparar o arquivo para download na memória
+        download_filename = original_filename.replace('.xml', '_indentado.xml')
+        download_file_content = BytesIO(formatted_xml.encode('utf-8'))
+        download_file_content.seek(0)
+    
+    if download_file_content:
+        return send_file(
+            download_file_content,
+            as_attachment=True,
+            download_name=download_filename,
+            mimetype='application/xml'
+        )
+    
+    return render_template('index.html', form=form)
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'xml_file' not in request.files:
-        return "Nenhum arquivo enviado!", 400
-    file = request.files['xml_file']
-    
-    if file.filename == '':
-        return "Nenhum arquivo selecionado!", 400
-    if not file.filename.endswith('.xml'):
-        return "Por favor, envie um arquivo XML!", 400
-    
-    original_filename = file.filename
-    filepath = os.path.join(app.config['UPLOAD_FOLDER'], original_filename)
-    file.save(filepath)
-    
-    tree = ET.parse(filepath)
-    root = tree.getroot()
-    
-    xml_string = StringIO()
-    ET.ElementTree(root).write(xml_string, encoding='unicode', xml_declaration=True)
-    xml_string.seek(0)
-    
-    xml_prettified = xml.dom.minidom.parseString(xml_string.getvalue())
-    formatted_xml = xml_prettified.toprettyxml(indent="  ")
-    
-    formatted_filename = original_filename.replace('.xml', '_indentado.xml')
-    formatted_filepath = os.path.join(app.config['UPLOAD_FOLDER'], formatted_filename)
-    with open(formatted_filepath, 'w', encoding='utf-8') as f:
-        f.write(formatted_xml)
-    
-    os.remove(filepath)
-    
-    return render_template('index.html', download_link=f'/download/{formatted_filename}')
-
-@app.route('/download/<filename>')
-def download_file(filename):
-    formatted_filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    return send_file(formatted_filepath, as_attachment=True, download_name=filename)
-
-# if __name__ == '__main__':
-#     app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
